c73f2dd2a94c796c1b82e6d3d6b9b11097bd953f
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-07 09:32:17 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,42 +12,42 @@ function setup() {
   createCanvas(600,200);
   
   
-  //create a trex sprite
+  //crear sprite de Trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
   trex.scale = 0.5;
   
-  //create a ground sprite
+  //crear sprite de suelo
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
   
-  //creating invisible ground
+  //crear sprite de suelo invisible
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
   
 }
 
 function draw() {
-  //set background color
+  //establecer color de fondo
   background(220);
   
   console.log(trex.y)
   
-  //jump when the space key is pressed
+  //hacer que el Trex salte al presionar la barra espaciadora
   if(keyDown("space") && trex.y >= 100) {
     trex.velocityY = -10;
   }
   
-  //add gravity
+  //agregar gravedad
   trex.velocityY = trex.velocityY + 0.8
   
   if (ground.x < 0){
     ground.x = ground.width/2;
   }
   
-  //stop trex from falling down
+  //evitar que el Trex caiga
   trex.collide(invisibleGround);
   
   drawSprites();
